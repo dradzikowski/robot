14d720a266f41e4ca247e995c9465ea22dcef602
dradzikowski <danielradzikowski@gmail.com>
2015-12-12 18:49:19 +0100

    Static fix

--- robot/settings.py
+++ robot/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from sys import path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,5 +104,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT =path.join(BASE_DIR,'static')
+#STATIC_ROOT =path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_URL =path.join(BASE_DIR,'static')
