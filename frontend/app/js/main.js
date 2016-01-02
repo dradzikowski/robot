bc8c7945491db2670fac188ebbbfaed61c49acd4
dradzikowski <danielradzikowski@gmail.com>
2016-01-02 11:46:15 +0100

    frontend changes

--- frontend/app/js/main.js
+++ frontend/app/js/main.js
@@ -1,20 +1,32 @@
-angular.module('main', [])
+angular.module('main', ['ngTagsInput', 'chart.js'])
 
     .config(function () {
     })
     .controller('MainController', ['$scope', '$filter', 'MainService', '$routeParams',
         function ($scope, $filter, mainService, $routeParams) {
+
             $scope.getstatistics = function () {
                 //key = $scope.key;
                 //value = $scope.value;
-                data = {"key": "keywords", "value": $scope.value};
+                $scope.loading = true;
+                data = {"key": "keywords", "value": $scope.value[0].value};
                 mainService.getstatistics(data, function (res) {
                     $scope.result = res.data;
-                    console.log(res );
+                    $scope.loading = false;
+                    console.log(res);
                 }, function (res) {
                     console.log(res);
                 })
-            }
+            };
+
+            /*
+            $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];*/
+
         }])
     .service('MainService', ['$http', function ($http) {
         var baseUrl = "http://localhost:8001";
